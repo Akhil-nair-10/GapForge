d013cf2e5fc554383ebb77422893ef5aa401f790
Akhil Nair <akhilgnair2006@gmail.com>
2026-09-12 13:36:57 +0530

    refixed resumePdf.js

--- BACKEND/src/utils/resumePdf.js
+++ BACKEND/src/utils/resumePdf.js
@@ -4,7 +4,7 @@ async function generateResumePdf(html) {
 
     const browser = await puppeteer.launch({
         headless: true,
-        executablePath: puppeteer.executablePath(),
+        executablePath: await puppeteer.executablePath(),
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox'
